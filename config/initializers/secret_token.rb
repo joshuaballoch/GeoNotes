a6dcf11651d27708a464d09bb8a757280bf6ad71
Joshua Balloch <joshuaballoch@gmail.com>
2012-03-25 13:17:58 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoNotes::Application.config.secret_token = '541a8e20cde9b98d4ff165fd4268f9abedff8f3b26160a3b13b6262c57db07b283d8a17b4c95ce91b0d5914ce7c8fa46bcb5be2b3f576a8639ab6b7e58310d7c'
