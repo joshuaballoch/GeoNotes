c94a8d80888e7b4790291f423217ce2354c90c1a
Joshua Balloch <joshuaballoch@gmail.com>
2012-03-27 23:35:31 -0400

    testing leaflet

---
+++ public/javascripts/maps_testing.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	var map = new L.Map('map');
+	var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/[c827a0eab8d845a6b31a791809e49933]/997/256/{z}/{x}/{y}.png', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+	    maxZoom: 18
+	});
+	var london = new L.LatLng(51.505, -0.09); // geographical point (longitude and latitude)
+	map.setView(london, 13).addLayer(cloudmade);
+});
